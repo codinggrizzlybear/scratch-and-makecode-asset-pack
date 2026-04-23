8827c0da77e726bed5a4faecc235830a1e04113e
codinggrizzlybear <130244126+codinggrizzlybear@users.noreply.github.com>
2026-04-23 07:48:42 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,217 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "Makecode Arcade":return img`
+..cccccccccccccccccccc..
+..cccccccccccccccccccc..
+..cc1cccccc11cccccc1cc..
+..c1cccccc1111cccccc1c..
+..c1cccccc1111cccccc1c..
+..c1ccccccc11ccccccc1c..
+..c1ccc1111111111ccc1c..
+.cc1ccc1111111111ccc1cc.
+cc1cccc1111111111cccc1cc
+c1ccccc1111111cc1ccccc1c
+cc1cccc111111cccccccc1cc
+.cc1ccc111111ccccccc1cc.
+..c1ccc1111111cc1ccc1c..
+..c1ccc1111111111ccc1c..
+..c1ccc1111111111ccc1c..
+..c1ccc1111111111ccc1c..
+..cc1cccccccccccccc1cc..
+..cccccccccccccccccccc..
+..cccccccccccccccccccc..
+`;
+            case "image3":
+            case "MicroBit":return img`
+................bdddddd..................
+.................bdddd...................
+...ffffffffffffffbddddfbddddddfffddddff..
+..fffffffffffffffbddddfbddcdddffddddddff.
+.ffbbbfbbbfbbbfffffffffbdcccddffddddddfff
+cffbfbbbfbbbfffffffffffbddcdddffddddddfff
+cffbfffffffffffffffffffbddddddffdfbbfdfff
+cfffffffffffffffffffffffffffffffdfbbfdfff
+cfbbfffffffffffffffffffffffffffffffffffff
+cfbbffcccccffffffffffffffffffffffffffffff
+cfbbffcccccffffffffffffffffffffffffffffff
+cfffffcccccffffffffffffffffffffffffffffff
+cfffffcccccfffffffffffffffffffffccccfffff
+cfffffcccccfffffffffffffffffffffccccfbbff
+cfffffffffffffffffffff1f1f1fffffccccfbbff
+cfffffffffffffffffffffffffffffffccccfffff
+cf1fffffffffffffffffff11111ffffffffffff1f
+cffffffffffffffffffff1fffff1fffffffffffff
+cffccffffffffffffffff1f1f1f1fffffffffffff
+cffccffffffffffffffff1fffff1fffffffffffff
+cfffffffffffffffffffff11111ffffffffffffff
+cffccffffffffffffffffffffffffffffffffffff
+cffccffffffffffffffffffffffffffffffffffff
+cffffffffffffffffffffffffffffffffffffffff
+cfff55ffffff55ffffff55ffffff55ffffff55fff
+cff5ff5ffff5ff5ffff5ff5ffff5ff5ffff5ff5ff
+cff5ff5ffff5ff5ffff5ff5ffff5ff5ffff5ff5ff
+cfff55ffffff55ffffff55ffffff55ffffff55fff
+c5f5555f55f5555f55f5555f55f5555f55f5555f5
+c5f5555f55f5555f55f5555f55f5555f55f5555f5
+ccf5555f55f5555f55f5555f55f5555f55f5555f.
+.cc5555f55f5555f55f5555f55f5555f55f5555..
+..cccccccccccccccccccccccccccccccccccc...
+`;
+            case "image1":
+            case "Scratch Cat":return img`
+........f.................
+.......f4f.........ff.....
+.......f44fffffffff44f....
+.......f4444444444444f....
+......fff44f444f44444f....
+.....f111f4f44f444444f....
+.....f111f4f4f44ff444f....
+....f1f11f44f44f11f44f....
+....f111f44444f111f44f....
+f..ffffff4444f1f11f444f...
+.ff11111ffffff111f4444f...
+..f1111111ff11fff14444f.f.
+..f111111111111111144fff..
+...f111f1111111f11144f....
+....ff11fffffff11114f.....
+......ff1111111ff14f......
+........fffffff44ff.....f.
+.......f..f44444444f...f1f
+......f4ff4444444444f.f11f
+......f444ff444444444f441f
+.......fff..f44444444444f.
+............f4444444444f..
+...........f44444444fff...
+..........f444fff444444f..
+.....fffff444f...fff444f..
+....f4444444f.....ff444f..
+....f444444f......f444f...
+.....ffffff........fff....
+`;
+            case "image4":
+            case "Scratch Logo":return img`
+. . . . . 4 4 4 
+. 4 4 4 4 4 1 4 
+. 4 1 1 1 1 1 4 
+. 4 1 4 4 4 1 4 
+. 4 1 4 4 4 4 4 
+4 4 1 1 1 1 4 . 
+4 4 4 4 4 1 4 . 
+4 1 4 4 4 1 4 . 
+4 1 1 1 1 1 4 . 
+4 1 4 4 4 4 4 . 
+4 4 4 . . . . . 
+`;
+            case "image5":
+            case "AdaBot":return img`
+. 6 . . . . . . . . . . . . 6 . 
+6 9 6 . . . . . . . . . . 6 9 6 
+. 6 . . . . . . . . . . . . 6 . 
+. 6 . . . . . . . . . . . . 6 . 
+. 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+9 6 9 1 1 1 9 9 9 9 1 1 1 9 6 9 
+9 6 9 1 1 f 9 9 9 9 1 f 1 9 6 9 
+9 6 9 1 f f 9 9 9 9 f f 1 9 6 9 
+9 6 9 9 9 9 9 9 9 9 9 9 9 9 6 9 
+. 6 9 9 6 1 1 1 1 1 1 6 9 9 6 . 
+. . 9 9 9 6 6 6 6 6 6 9 9 9 . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+`;
+            case "image6":
+            case "Blinka":return img`
+. . . . . . a a a a . . . . . . 
+. . . . . a a a 1 9 a . . . . . 
+. . . . . a a a 1 8 a a . . . . 
+. . . . . a a a a a a c . . . . 
+. . . . . a a a 3 . . . 3 . . . 
+. . . . . . a a a 9 . . . . . . 
+. . . . . . . a a a 3 . . . . . 
+. . . . . . . . a a 9 9 . . . . 
+. . . . . . . . . a a 3 3 . . . 
+. . . . a a a a c c a 9 9 . . . 
+. . . 9 3 9 3 9 3 c a a 3 3 . . 
+. . a a a a a c c c a a 9 9 . . 
+. . 9 3 9 3 9 3 9 c a a 3 3 . . 
+a a a a a a c c c c c c 9 9 . . 
+a a a a a a a a a a a c c c . a 
+3 9 3 9 3 9 3 9 3 a a a a a a . 
+`;
+            case "image7":
+            case "Mho":return img`
+. . . d 4 d . . . . d 5 d . . . 
+. . d d 1 d f d e d d 1 d d . . 
+c c d 1 f 1 f 1 1 d 1 f 1 d c c 
+c . d f f d f d e d f f d d . c 
+c . . d 4 d . . . . d 5 d . . c 
+c . . . . . . . . . . . . . . c 
+`;
+            case "image8":
+            case "Minerva":return img`
+. . . . 3 . . . . . . . . . . 
+. . . . . 3 3 . . 3 . . . . . 
+. . . . . . 3 3 3 . . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. 3 3 3 3 8 8 8 8 8 3 3 3 3 . 
+3 9 9 9 9 3 8 8 8 3 9 9 9 9 3 
+9 c c 8 8 9 3 8 3 9 8 8 c c 9 
+c 8 8 1 1 8 9 3 9 8 1 1 8 8 c 
+a 8 1 9 f 1 8 9 8 1 9 f 1 8 a 
+a 8 1 f f 1 8 c 8 1 f f 1 8 a 
+a a 8 1 1 8 c 9 c 8 1 1 8 a a 
+a a a 8 8 a a a a a 8 8 a a a 
+a a a a a a a a a a a a a a a 
+8 a 8 a 8 a 8 a 8 a 8 a 8 a 8 
+. 8 . 8 . 8 . 8 . 8 . 8 . 8 . 
+`;
+            case "image9":
+            case "Sparky":return img`
+. 6 6 . . . . . . . . 
+. . 6 6 . . . . . . . 
+. . 6 6 6 . . . . 6 . 
+. . 6 6 6 . . . 6 . . 
+. . 6 6 6 6 . 6 6 . . 
+. 6 6 6 6 6 6 6 6 . . 
+. 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 . 
+6 1 1 9 6 6 6 9 1 1 6 
+6 6 1 8 1 6 1 8 1 6 6 
+6 6 6 8 6 6 6 8 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 1 6 1 6 1 6 6 6 
+. 6 1 6 1 6 1 6 1 6 . 
+. . 6 6 6 6 6 6 6 . . 
+. . . 6 6 6 6 6 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
